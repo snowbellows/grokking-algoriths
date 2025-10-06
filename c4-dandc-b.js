cc1618e14149aa05f7d38d372bfdf28568ca3841
Alex Halverson <alexhalverson@posteo.net>
2025-10-06 11:15:58 +1100

    add chapter 4 example b

---
+++ c4-dandc-b.js
@@ -0,0 +1,11 @@
+function quicksort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const pivot = arr[0];
+  const less = arr.slice(1).filter((x) => x <= pivot);
+  const greater = arr.slice(1).filter((x) => x >= pivot);
+  return [...quicksort(less), pivot, ...quicksort(greater)];
+}
+
+console.log(quicksort([10, 5, 2, 3]));
